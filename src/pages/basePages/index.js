1a727b3f75c4d7118f503b843fe5d9be600ba5e7
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-11-22 19:19:36 +0700

    add blog edit page route

--- src/pages/basePages/index.js
+++ src/pages/basePages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Home";
+import BlogEdit from "../BlogEdit";
 
 function BasePage() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="edit" element={<BlogEdit />} />
       </Routes>
     </div>
   );
