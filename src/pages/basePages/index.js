2290fb1895eb759caa23bd2b08400fc92da206f3
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-07 18:05:09 +0700

    add navbar

--- src/pages/basePages/index.js
+++ src/pages/basePages/index.js
@@ -3,14 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../authPages/LoginPage";
 import Home from "../Home";
 import BlogEdit from "../BlogEdit";
-
+import NavBar from "../../components/NavBar/index";
 //URL: localhost:{$port}/auth/{$authPageURL}
 
 //$port: we can config.
 //$authPageURLs are defined below.
 function BasePage() {
   return (
-    <div>
+    <div className="container">
+      <NavBar />
+      <div style={{marginTop: 80}}></div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="edit" element={<BlogEdit />} />
