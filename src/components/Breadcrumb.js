1c5f27263bdeb2f1aa1c7fbe4bd11599cfca6966
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-07 18:03:54 +0700

    add divider between title and path

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -4,28 +4,28 @@ function Breadcrumb(props) {
   const { title, parent, parent2, parent3, active } = props;
 
   return (
-    <div className="row">
+    <div className="row my-3">
       <div className="col-12 mb-2">
-        <h2 className="float-start me-4 mb-0">{title}</h2>
+        <h2 className="float-start me-4 pe-4 mb-0 border-2 border-secondary border-end">{title}</h2>
         <nav aria-label="breadcrumb" className="d-flex align-items-center h-100">
-          <ol class="breadcrumb mb-0">
+          <ol className="breadcrumb mb-0">
             {parent && (
-              <li class="breadcrumb-item">
+              <li className="breadcrumb-item">
                 <a href="#">{parent}</a>
               </li>
             )}
             {parent2 && (
-              <li class="breadcrumb-item">
+              <li className="breadcrumb-item">
                 <a href="#">{parent2}</a>
               </li>
             )}
             {parent3 && (
-              <li class="breadcrumb-item">
+              <li className="breadcrumb-item">
                 <a href="#">{parent3}</a>
               </li>
             )}
             {active && (
-              <li class="breadcrumb-item active" aria-current="page">
+              <li className="breadcrumb-item active" aria-current="page">
                 {active}
               </li>
             )}
