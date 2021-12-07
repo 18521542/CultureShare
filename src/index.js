52adf0d7a0f7cf42f236fe221519658675a787d4
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-07 18:02:30 +0700

    add flag icon, jquery, popper

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "flag-icon-css/css/flag-icons.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
