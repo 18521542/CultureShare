1c38cc17a9e57c5ffd2caf86d7a00b97daecc232
Duy Trong <tranduytrong2000@gmail.com>
2021-12-15 16:44:27 +0700

    finish merge with dieu's branch

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
-import UserPage from "../pages/userPages"
+import UserPage from "../pages/userPages/UserPage"
+import { AddCommentForm } from "../components/Comment/AddCommentForm";
 
 export const UserRoutes = () => {
     return (
         <Routes>
             <Route path="/user/*" element={<UserPage/>}/>
+            <Route path="/comment" element={<AddCommentForm/>}/>
         </Routes>
     )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import BasePage from "../pages/basePages"
-import UserPage from "../pages/userPages"
 import { PostRoutes } from "./PostRoutes";
 import { AuthRoutes } from "./LoginRoutes";
 import { UserRoutes } from "./UserRoutes";
