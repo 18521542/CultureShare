e884e3bb8afb94497cb4ceaf6b8764c0a8bbd642
Duy Trong <tranduytrong2000@gmail.com>
2021-12-15 20:32:03 +0700

    initialize Posts-Page

---
+++ src/pages/postPages/PostsPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function PostsPage(props) {
+    console.log(props)
+    return <div>Post List</div>;
+}
+
+export default PostsPage;
--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import BlogEdit from "../pages/BlogEdit"
 import Home from "../pages/Home"
+import PostsPage from "../pages/postPages/PostsPage"
 
 export const PostRoutes = () => {
     const postRoute = "/Post"
@@ -8,6 +9,7 @@ export const PostRoutes = () => {
         <Routes>
             <Route exact path={`${postRoute}`} element={<Home />} />
             <Route path={`${postRoute}/edit`} element={<BlogEdit />} />
+            <Route path={`${postRoute}/list`} element={<PostsPage/>}/>
         </Routes>
     )
 }
