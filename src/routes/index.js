ac59719859a8674dfa58aba3cba40591818ff43b
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-16 11:02:48 +0700

    fix route, separate routes and pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import BasePage from "../pages/basePages"
-import UserPage from "../pages/userPages"
-import { PostRoutes } from "./PostRoutes";
-import { AuthRoutes } from "./LoginRoutes";
-import { UserRoutes } from "./UserRoutes";
+import LoginPage from "../pages/authPages/LoginPage";
+import RegisterPage from "../pages/authPages/RegisterPage";
+import UserPage from "../pages/userPages";
+import Home from "../pages/Home";
+import CommonPage from "../pages/CommonPage";
 
-export const RootRoute = (props) => {
-    return (
-        <BrowserRouter>
-            <UserRoutes/>
-            <PostRoutes/>
-            <AuthRoutes/>
-      </BrowserRouter>
-    )
-}
+export const RootRoute = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth">
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/auth/register" element={<RegisterPage />} />
+        </Route>
+        <Route path="/" element={<CommonPage />}>
+          {/* put all pages that use common page layout here */}
+          <Route exact path="/" element={<Home />} />
+          <Route path="/user" element={<UserPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
