752d03c5f8ef50339ae72ca67ad5f5a316ec6b59
linhleba <linhleba.it@gmail.com>
2021-12-16 18:22:55 +0700

    update & merge homepage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
-import { PostRoutes } from "./PostRoutes";
-import { AuthRoutes } from "./LoginRoutes";
-import { UserRoutes } from "./UserRoutes";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { PostRoutes } from './PostRoutes';
+import { AuthRoutes } from './LoginRoutes';
+import { UserRoutes } from './UserRoutes';
+import { HomeRoutes } from './HomeRoutes';
 
 export const RootRoute = (props) => {
-    return (
-        <BrowserRouter>
-            <UserRoutes/>
-            <PostRoutes/>
-            <AuthRoutes/>
-      </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <UserRoutes />
+      <PostRoutes />
+      <AuthRoutes />
+      <HomeRoutes />
+    </BrowserRouter>
+  );
+};
