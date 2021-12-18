4862b3ae02870181f3187565be071b65d2484c5d
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-18 16:42:16 +0700

    add footer

--- src/pages/CommonPage.js
+++ src/pages/CommonPage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 // layout for all common pages except login and register pages
 function CommonPage() {
   return (
     <div className="container">
       <NavBar />
-      <div style={{ marginTop: 80 }} className="layout-wapper">
+      <div style={{ marginTop: 90 }} className="layout-wapper mb-4">
         <Outlet/>
       </div>
+      <Footer />
     </div>
   );
 }
