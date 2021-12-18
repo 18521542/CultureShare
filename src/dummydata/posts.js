c32eda23915474f8b3d26a38276bf7afd424effa
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-18 16:39:09 +0700

    data for threads

---
+++ src/dummydata/posts.js
@@ -0,0 +1,254 @@
+export const threads = [
+  {
+    threadId: "france",
+    title: "Pháp",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Pháp 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Pháp 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Pháp 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+  {
+    threadId: "greece",
+    title: "Hy Lạp",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Hy Lạp 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Hy Lạp 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Hy Lạp 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+  {
+    threadId: "italy",
+    title: "Ý",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Ý 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Ý 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Ý 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+  {
+    threadId: "vietnam",
+    title: "Việt Nam",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Việt Nam 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Việt Nam 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Việt Nam 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+  {
+    threadId: "japan",
+    title: "Nhật Bản",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Nhật Bản 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Nhật Bản 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Nhật Bản 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+  {
+    threadId: "singapore",
+    title: "Singapore",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Singapore 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Singapore 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Singapore 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+  {
+    threadId: "thailand",
+    title: "Thái Lan",
+    posts: [
+      {
+        id: 1,
+        title: "Các vấn đề hiện đại của Thái Lan 1",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Các vấn đề hiện đại của Thái Lan 2",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Các vấn đề hiện đại của Thái Lan 3",
+        description: "Một nụ cười luôn hé, thế giới vẫn quay, còn tôi vẫn nơi đây đêm nằm mơ,...",
+        user: {
+          name: "Miss Black",
+          avatar:
+            "/img/avatar-1.png"
+        }
+      },
+    ]
+  },
+];
