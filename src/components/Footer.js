a6c0a839925ca4a0e652e8ad1ea616e58dd0c388
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-18 16:34:28 +0700

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="d-flex flex-row align-items-center justify-content-lg-between w-100">
+      <p className="culture-font fs-3 me-2">Culture Share</p> <p>©2021 Created by Team 1</p>
+    </div>
+  );
+}
+
+export default Footer;
