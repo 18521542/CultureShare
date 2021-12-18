ce2c41982e8ef20660579094ff9476da585722ef
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-18 16:38:36 +0700

    change 'class' to 'className'

--- src/components/NavBar/DropdownUser.js
+++ src/components/NavBar/DropdownUser.js
@@ -4,53 +4,53 @@ function DropdownUser() {
   return (
     <div className="dropdown">
       <a
-        class="btn btn-light d-flex align-items-center"
+        className="btn btn-light d-flex align-items-center"
         id="dropdown-user"
         data-bs-toggle="dropdown"
         aria-haspopup="true"
         aria-expanded="false">
-        <div class="d-sm-flex flex-column d-none align-items-end">
-          <span class="fw-bolder">Jenny</span>
-          <span class="fw-light" style={{fontSize: 13}}>Member</span>
+        <div className="d-sm-flex flex-column d-none align-items-end">
+          <span className="fw-bolder">Jenny</span>
+          <span className="fw-light" style={{fontSize: 13}}>Member</span>
         </div>
         <div className="position-relative">
           <img
-            class="rounded-circle ms-2"
-            src="./img/avatar-1.png"
+            className="rounded-circle ms-2"
+            src="/img/avatar-1.png"
             alt="avatar"
             height="40"
             width="40"
           />
           <span
-            class="position-absolute bottom-0 end-0 rounded-circle"
+            className="position-absolute bottom-0 end-0 rounded-circle"
             style={{ height: 10, width: 10, backgroundColor: "#28c76f" }}></span>
         </div>
       </a>
-      <div class="dropdown-menu" aria-labelledby="dropdown-user">
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="user"></i> Profile
+      <div className="dropdown-menu" aria-labelledby="dropdown-user">
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="user"></i> Profile
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="mail"></i> Inbox
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="mail"></i> Inbox
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="check-square"></i> Task
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="check-square"></i> Task
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="message-square"></i> Chats
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="message-square"></i> Chats
         </a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="settings"></i> Settings
+        <div className="dropdown-divider"></div>
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="settings"></i> Settings
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="credit-card"></i> Pricing
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="credit-card"></i> Pricing
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="help-circle"></i> FAQ
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="help-circle"></i> FAQ
         </a>
-        <a class="dropdown-item" href="#">
-          <i class="mr-50" data-feather="power"></i> Logout
+        <a className="dropdown-item" href="#">
+          <i className="mr-50" data-feather="power"></i> Logout
         </a>
       </div>
     </div>
