71b17b60810e1ef77c992eb0ce6f9b64a8913170
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-18 16:38:50 +0700

    update navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,25 +1,59 @@
 import React from "react";
-import LeftItems from "./LeftItems";
-import RightItems from "./RightItems";
+import { Link } from "react-router-dom";
 import useToggle from "../../utils/hooks/useToggle";
-import SearchBar from "./SearchBar";
+import DropdownNotification from "./DropdownNotification";
+import DropdownLanguage from "./DropdownLanguage";
+import DropdownUser from "./DropdownUser";
 
 function NavBar() {
   const [isOpen, toggle] = useToggle(false);
   console.log(isOpen);
   return (
-    <nav className="container-fluid header-navbar navbar align-items-center fixed-top navbar-expand-lg navbar-light">
-      <div
-        className="shadow d-flex justify-content-between bg-light w-100 rounded px-3"
-        style={{ height: 62 }}>
-        {isOpen ? (
-          <SearchBar isOpen={isOpen} toggle={toggle} />
-        ) : (
-          <div className="d-flex justify-content-between w-100">
-            <LeftItems />
-            <RightItems toggle={toggle} />
-          </div>
-        )}
+    <nav className="fixed-top navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <Link className="navbar-brand culture-font" to="/">
+            Culture Share
+          </Link>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <div className="d-flex input-group">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <DropdownNotification />
+        <DropdownLanguage />
+        <DropdownUser />
       </div>
     </nav>
   );
